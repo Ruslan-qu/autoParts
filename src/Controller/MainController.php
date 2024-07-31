3cbee7c479ebf721ae67c6241a27e781b0cb3e81
qu <quqichbakich@mail.ru>
2024-07-31 21:44:09 +0300

    bootstrap

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'main_page')]
+    public function index(): Response
+    {
+        return $this->render('main/main.html.twig', [
+            'main_page' => 'Бугалтерия начало))))',
+            'title_logo' => 'Главная'
+        ]);
+    }
+}
